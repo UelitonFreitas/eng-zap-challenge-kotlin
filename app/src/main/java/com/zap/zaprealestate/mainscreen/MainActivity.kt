35835d21d55040b3400075efbb4109421a5b68e2
uelitonfreitas <freitas.ueliton@gmail.com>
2021-06-13 19:19:23 -0300

    feat: extract Zap properties to fragment

--- app/src/main/java/com/zap/zaprealestate/mainscreen/MainActivity.kt
+++ app/src/main/java/com/zap/zaprealestate/mainscreen/MainActivity.kt
@@ -1,27 +1,81 @@
 package com.zap.zaprealestate.mainscreen
 
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import androidx.viewpager2.widget.ViewPager2
 import com.zap.zaprealestate.R
 import com.zap.zaprealestate.mainscreen.adpters.PropertiesAdapter
-import com.zap.zaprealestate.model.remote.PropertiesRepositoryImpl
 import com.zap.zaprealestate.model.Property
+import com.zap.zaprealestate.model.remote.PropertiesRepositoryImpl
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity(), MainScreenProtocols.View {
+class MainActivity : AppCompatActivity() {
 
-    private val propertiesList by lazy { findViewById<RecyclerView>(R.id.recycler_view_properties) }
-    private val swipeLayout by lazy { findViewById<SwipeRefreshLayout>(R.id.swipe_container) }
-    private val presenter = ZapScreenPresenter(this, PropertiesRepositoryImpl())
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        presenter.getPropertiesList()
+
+        pager.adapter = DemoCollectionAdapter(this)
+        pager.orientation = ViewPager2.ORIENTATION_HORIZONTAL
+        pager.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
+            override fun onPageSelected(position: Int) {
+                super.onPageSelected(position)
+                Toast.makeText(this@MainActivity, "onPageSelected", Toast.LENGTH_SHORT).show()
+            }
+        })
+    }
+}
+
+class DemoCollectionAdapter(appCompatActivity: AppCompatActivity) :
+    FragmentStateAdapter(appCompatActivity) {
+
+    override fun getItemCount(): Int = 2
+
+    override fun createFragment(position: Int): Fragment {
+        // Return a NEW fragment instance in createFragment(int)
+        val fragment = ZapFragment()
+        fragment.arguments = Bundle().apply {
+            // Our object is just an integer :-P
+            putInt(ARG_OBJECT, position + 1)
+        }
+        return fragment
+    }
+}
+
+private const val ARG_OBJECT = "object"
+
+// Instances of this class are fragments representing a single
+// object in our collection.
+class ZapFragment : Fragment(), MainScreenProtocols.View {
+
+    private lateinit var propertiesList: RecyclerView
+    private lateinit var swipeLayout: SwipeRefreshLayout
+    private lateinit var presenter: ZapScreenPresenter
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return inflater.inflate(R.layout.fragment_collection_object, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        propertiesList = view.findViewById<RecyclerView>(R.id.recycler_view_properties)
+        swipeLayout = view.findViewById<SwipeRefreshLayout>(R.id.swipe_container)
+        presenter = ZapScreenPresenter(this, PropertiesRepositoryImpl())
 
         swipeLayout.setOnRefreshListener {
             presenter.getPropertiesList()
@@ -33,6 +87,7 @@ class MainActivity : AppCompatActivity(), MainScreenProtocols.View {
             android.R.color.holo_red_light)
     }
 
+
     override fun onResume() {
         super.onResume()
 
@@ -41,7 +96,7 @@ class MainActivity : AppCompatActivity(), MainScreenProtocols.View {
 
     override fun showProperties(properties: List<Property>) {
         val viewAdapter = PropertiesAdapter(properties)
-        val viewManager = LinearLayoutManager(this)
+        val viewManager = LinearLayoutManager(this.activity)
 
         propertiesList.apply {
             layoutManager = viewManager
@@ -62,11 +117,11 @@ class MainActivity : AppCompatActivity(), MainScreenProtocols.View {
     }
 
     override fun showEmptyList() {
-        Toast.makeText(this, "Empty List", Toast.LENGTH_SHORT).show()
+        Toast.makeText(this.activity, "Empty List", Toast.LENGTH_SHORT).show()
     }
 
     override fun showErrorMessage() {
-        Toast.makeText(this, "Error when loading List", Toast.LENGTH_SHORT).show()
+        Toast.makeText(this.activity, "Error when loading List", Toast.LENGTH_SHORT).show()
     }
 
     override fun showLoading() {
